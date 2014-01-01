2321ca8a66036ca4e42f789e5ae576af79504a49
Alan Monger <haveacigaro@gmail.com>
2014-01-01 13:41:35 +0000

    Adds namespaces to all classes and puts them in psr-0 folders

--- Bitcoin/JsonRPCClient.php
+++ Bitcoin/JsonRPCClient.php
@@ -27,6 +27,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @author sergio <jsonrpcphp@inservibile.org>
  */
+
+namespace Bitcoin;
+
 class JsonRPCClient {
 	
 	/**
